6ccb209ed9d362ef03a285349a5101c30ac56aa0
molletta <yasyule4ka@mail.ru>
2023-07-28 19:25:45 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function toReadable (number) {
-  
+    const oneDigit = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const twoDigit = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const dez = ['', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    let numStr = number.toString();
+    let result = '';
+
+    if (number === 0) {return 'zero'}
+
+    if(numStr.length == 1 && number > 0) { result = oneDigit[number - 1]}
+    else if(numStr.length == 2) { if (numStr[0] == '1') {result = twoDigit[+numStr[1]]}
+                                  else { result = numStr[1] == 0 ? dez[+numStr[0] - 1] : dez[+numStr[0] - 1] + ' ' + oneDigit[+numStr[1] - 1]}
+                                 }
+    else if(numStr.length == 3) {
+
+    let twoDigitRead = '';
+
+    if (numStr[1] == '0' && numStr[2] == '0') {twoDigitRead = '' }
+    else if (numStr[1] == '0' && numStr[2] != '0') {twoDigitRead = '' + oneDigit[+numStr[2] - 1]}
+    else if (numStr[1] == '1') {twoDigitRead = twoDigit[+numStr[2]]}
+    else {twoDigitRead = numStr[2] == 0 ? dez[+numStr[1] - 1] : dez[+numStr[1] - 1] + ' ' + oneDigit[+numStr[2] - 1]};
+
+    result = oneDigit[+numStr[0]-1] + ' hundred ' + twoDigitRead;
+    }
+
+    result = result.trim()
+
+    return result
+
 }
